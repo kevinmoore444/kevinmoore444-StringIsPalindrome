a92fde3758b22050a0adf892e74340dbbf215c6d
Kevin Moore <kevinmoore444@gmail.com>
2023-07-13 22:45:48 +0000

    commit

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -3,7 +3,7 @@ public class Palindrome {
     /**
      * Palindromes are words that are the same forwards as they are backwards - eg, 'bob', 'racecar'.
      *
-     * You can solve this problem in several ways
+     * You can solve this problem in several ways:
      * You could start with a String reversal algorithm, which you have already written, and check if str is the same
      * as its reversed form, or, you could check if the character at the nth position of a String moving forwards
      * re the same as the nth position of a String moving backwards.
